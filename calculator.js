d7b3d4499d60a2a765385f4610384d5af743c5d5
Konstantin <st4nshashkov@yandex.ru>
2025-03-28 12:51:08 +0300

    js fixing backspace

--- calculator.js
+++ calculator.js
@@ -2,7 +2,13 @@ $(document).ready(function(){
     //take html tags with assigned values and set to variable.
     //set textarea to the value of "blank", and add to values.
     
-     $('#1,#2,#3,#4,#5,#6,#7,#8,#9,#0,#add, #subtract, #multiply, #divide, #power, #dot, #para1, #para2').click(function(){
+    if(/Android/.test(navigator.userAgent)){
+      $('backspace').html(`<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 25 25" height="24" width="24"><path style="fill:#f5f5f5"  d="M24 12.001H2.914l5.294-5.295-.707-.707L1 12.501l6.5 6.5.707-.707-5.293-5.293H24v-1z" data-name="Left"/></svg>`)
+    } else {
+      $('backspace').html('&#x2190;')
+    }
+
+    $('#1,#2,#3,#4,#5,#6,#7,#8,#9,#0,#add, #subtract, #multiply, #divide, #power, #dot, #para1, #para2').click(function(){
           var v = $(this).val();
       var total = $('textarea').val($('textarea').val() + v); 
       });
@@ -21,4 +27,6 @@ $(document).ready(function(){
     $('#backspace').click(function(){
         $('textarea').val($('textarea').val().substring(0, $('textarea').val().length - 1));
       });
+
+    
   });
