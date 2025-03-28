f8dc3956ddf421fc4cecc38bbadd64a36a138237
Konstantin <st4nshashkov@yandex.ru>
2025-03-28 12:59:49 +0300

    again

--- calculator.js
+++ calculator.js
@@ -3,9 +3,13 @@ $(document).ready(function(){
     //set textarea to the value of "blank", and add to values.
     
     if(/Android/.test(navigator.userAgent)){
-      $('backspace').html(`<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 25 25" height="24" width="24"><path style="fill:#f5f5f5"  d="M24 12.001H2.914l5.294-5.295-.707-.707L1 12.501l6.5 6.5.707-.707-5.293-5.293H24v-1z" data-name="Left"/></svg>`)
+      $('#backspace').html(`<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 25 25" height="24" width="24"><path style="fill:#f5f5f5"  d="M24 12.001H2.914l5.294-5.295-.707-.707L1 12.501l6.5 6.5.707-.707-5.293-5.293H24v-1z" data-name="Left"/></svg>`)
+      console.log('SVG')
     } else {
-      $('backspace').html('&#x2190;')
+      console.log('HTML code')
+      $('#backspace').html('&#x2190;')
+      $('#navigator').css("color","white")
+      $('#navigator').text(`${navigator.userAgent}`)
     }
 
     $('#1,#2,#3,#4,#5,#6,#7,#8,#9,#0,#add, #subtract, #multiply, #divide, #power, #dot, #para1, #para2').click(function(){
